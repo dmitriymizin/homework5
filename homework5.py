6417f03c336919cfd3d615f9ea7c4a0a0012f8ac
dmitriymizin <169042072+dmitriymizin@users.noreply.github.com>
2024-05-10 16:47:05 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list=["apple", "orange", "cherry", "kiwi", "grape", "plum"]
+print(my_list)
+print(my_list[0]) # первый элемент
+print(my_list[-1]) # последний элемент
+print(my_list[2:5]) # подсписок
+my_list[2]='coconut' # заменили элемент
+print(my_list)
+
+my_dict={'apple': 'яблоко', 'orange': 'апельсин', 'cherry': 'вишня'}
+print(my_dict)
+print(my_dict['apple']) # значение элементе
+my_dict['apple']='запретный плод' # сменили значение элемента
+print(my_dict)
